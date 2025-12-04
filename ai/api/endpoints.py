e179bd8c24f68def4a9cc4a31aecc29154d70d55
flisim <eduardermakov769@gmail.com>
2025-12-04 20:42:42 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os      ZOLENO
+import os      flisim
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
